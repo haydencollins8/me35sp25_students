dbf3878c9b0605aa0fe27adbca0b77dcc115a1e3
Briana Bouchard <briana.bouchard@tufts.edu>
2025-02-06 12:43:28 -0500

    Add DC motor and IR sensor code and wiring

---
+++ SampleCode/IRLineSensor.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+sensor_pin = 11
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor_pin, GPIO.IN)
+
+try:
+    while True:
+        sensor_value = GPIO.input(sensor_pin)
+        print("Sensor reading:", sensor_value)
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
